4b32e05d44f8c08f1bc1ab362a8d65d3dba07c89
João Mauricio <jmauricio@pobox.com>
2018-06-25 21:09:45 -0300

    Eclipse - fixing log

--- provision/0030-eclipse.sh
+++ provision/0030-eclipse.sh
@@ -3,8 +3,8 @@ mkdir -p /usr/java-env/ide/eclipse
 
 sudo apt-get install -y libcanberra-gtk-module libcanberra-gtk3-module
 
-echo === Eclipse - $url
 url=http://mirror.nbtelecom.com.br/eclipse/technology/epp/downloads/release/oxygen/3a/eclipse-jee-oxygen-3a-linux-gtk-x86_64.tar.gz
+echo === Eclipse - $url
 curl -L $url 2> /dev/null | tar xz -C /usr/java-env/ide/eclipse
 
 cd /usr/java-env/ide/eclipse
