73f328b8c6b99e685547c012519b457da0881a5c
João Mauricio <jmauricio@pobox.com>
2018-06-27 08:31:31 -0300

    VSCode - redirect stderr to dev/null

--- provision/0020-vscode.sh
+++ provision/0020-vscode.sh
@@ -1,5 +1,5 @@
 echo === VSCode
-curl -L https://go.microsoft.com/fwlink/?LinkID=760868 -o /tmp/vscode.deb 
+curl -L https://go.microsoft.com/fwlink/?LinkID=760868 -o /tmp/vscode.deb 2>/dev/null
 sudo apt-get install -y /tmp/vscode.deb libgtk2.0-0 libxss1 libasound2
 rm -f /tmp/vscode.deb
 
