7ac8eb4e3d9ff501be35ec3d877cd4f5d39fb096
João Mauricio de O. Alves <jmauricio@pobox.com>
2019-08-27 23:50:17 -0300

    Adding cordova

--- provision/0080-ionic.sh
+++ provision/0080-ionic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo -iu vagrant yarn global add ionic
+sudo -iu vagrant yarn global add ionic cordova
 sudo -iu vagrant ionic config set -g yarn true
 sudo -iu vagrant ionic config set -g npmClient yarn
