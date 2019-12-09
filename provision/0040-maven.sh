2fe450df11a634925d2ec5a28700c3435d4332d9
João Mauricio de O. Alves <jmauricio@pobox.com>
2019-12-09 07:49:00 -0300

    Fix (why?) gunzip error in tar xz

--- provision/0040-maven.sh
+++ provision/0040-maven.sh
@@ -6,7 +6,7 @@ url=https://www-us.apache.org/dist/maven/maven-3/$version/binaries/apache-maven-
 echo $url
 
 mkdir -p /usr/lib/maven
-curl -L $url 2> /dev/null | tar xz -C /usr/lib/maven
+curl -L $url 2> /dev/null | gunzip | tar x -C /usr/lib/maven
 
 cd /usr/lib/maven
 ln -s $( ls -1d apache-maven-* | tail -1 ) latest
