00d6f7e20157ba1ea54a21a92e920708f01066eb
João Mauricio de O. Alves <jmauricio@pobox.com>
2019-12-09 22:49:32 -0300

    Fix (why?) xz error in tar xJ

--- provision/0060-node.sh
+++ provision/0060-node.sh
@@ -7,7 +7,7 @@ url=https://nodejs.org/dist/v$version/node-v$version-linux-x64.tar.xz
 echo $url
 mkdir -p /usr/lib/node
 
-curl -L $url 2> /dev/null | xz | tar x -C /usr/lib/node
+curl -L $url 2> /dev/null | xz --decompress --stdout | tar x -C /usr/lib/node
 
 cd /usr/lib/node
 ln -s $( ls -1d node-* | tail -1 ) latest
