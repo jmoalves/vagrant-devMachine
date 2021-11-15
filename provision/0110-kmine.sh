3e5691fa8427773d69f117673c6a3e7d95429aef
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2021-11-15 11:49:25 -0300

    knime - download latest

--- provision/0110-kmine.sh
+++ provision/0110-kmine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-knimeFile=../kit/knime_4.4.2.linux.gtk.x86_64.tar.gz
+url="https://download.knime.org/analytics-platform/linux/knime-latest-linux.gtk.x86_64.tar.gz"
 
 sudo -iu vagrant mkdir -p /media/sf_storage/knime-workspace
 sudo -iu vagrant ln -s /media/sf_storage/knime-workspace ~vagrant/
@@ -10,8 +10,8 @@ sudo apt-get install -y libcanberra-gtk-module libcanberra-gtk3-module
 
 mkdir -p /usr/lib/knime
 
-echo $knimeFile
-curl -L "$knimeFile" 2> /dev/null | tar xz -C /usr/lib/knime
+echo $url
+curl -L "$url" 2> /dev/null | tar xz -C /usr/lib/knime
 
 cd /usr/lib/knime
 ln -s $( ls -1d knime* | tail -1 ) latest
