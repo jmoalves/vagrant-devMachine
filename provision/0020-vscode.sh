af939e8a65b7b75824bcea81d55b2bedfdccf855
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2021-11-16 16:32:09 -0300

    vscode - java + docker + kubernetes

--- provision/0020-vscode.sh
+++ provision/0020-vscode.sh
@@ -4,3 +4,11 @@ curl -L https://go.microsoft.com/fwlink/?LinkID=760868 -o /tmp/vscode.deb 2>/dev
 sudo apt-get update
 sudo apt-get install -y -f /tmp/vscode.deb libxss1
 rm -f /tmp/vscode.deb
+
+
+### Extensions
+
+code --force \
+    --install-extension vscjava.vscode-java-pack \
+    --install-extension ms-azuretools.vscode-docker \
+    --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
