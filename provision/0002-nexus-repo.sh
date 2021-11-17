f5de706d56c996bbdd19c80fc85516eca0b4f1b0
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2021-11-17 15:02:49 -0300

    nexus-repos - check if already configured

--- provision/0002-nexus-repo.sh
+++ provision/0002-nexus-repo.sh
@@ -3,9 +3,13 @@
 nexusRepo=http://192.168.15.200:8081/repository/ubuntu-focal/
 nexusStatus=$( curl -o /dev/null -s -w "%{http_code}\n" ${nexusRepo} )	
 if [ $nexusStatus == 200 ]; then
-    echo === Nexus APT Proxy for Ubuntu found
-    sudo cp /etc/apt/sources.list /etc/apt/sources.list.orig && \
-        cat /etc/apt/sources.list.orig \
-        | sed "s@http://.*\.?archive\.ubuntu\.com/ubuntu@${nexusRepo}@g" \
-        > /etc/apt/sources.list
+    if grep -qv "$nexusRepo" /etc/apt/sources.list; then
+        echo === Nexus APT Proxy for Ubuntu found
+        sudo cp /etc/apt/sources.list /etc/apt/sources.list.orig && \
+            cat /etc/apt/sources.list.orig \
+            | sed "s@http://.*\.?archive\.ubuntu\.com/ubuntu@${nexusRepo}@g" \
+            > /etc/apt/sources.list
+    else
+        echo === Nexus APT Proxy for Ubuntu already configured
+    fi
 fi
